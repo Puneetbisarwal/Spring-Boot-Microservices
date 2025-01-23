a0b8d87bc3eddd24992e9fa24e65f36a6cbe3458
Puneetbisarwal <puneetbisarwal@gmail.com>
2025-01-23 19:43:21 +0530

    Added Eureka Server in movie catalog service, movie info service, ratings data service

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,6 +1,7 @@
 package io.javabrains.moviecatalogservice.resources;
 
 
+import com.netflix.discovery.DiscoveryClient;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
@@ -26,16 +27,19 @@ public class MovieCatalogResource {
     private RestTemplate restTemplate;
 
     @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
     private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         // get all rated movie IDs
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
             // For each movie ID,call movie info service and get details
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             // Put them all together
             return new CatalogItem(movie.getName(), "Description", rating.getRating());
 
--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,13 +2,14 @@ package io.javabrains.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class MovieInfoServiceApplication {
+@EnableEurekaClient
+class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
 	}
 
 }
-
